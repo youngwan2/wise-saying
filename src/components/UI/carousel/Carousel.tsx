df3ccd1561f888a8d394643147bdf3b42cc50d79
Kim Young Wan <qodna25@gmail.com>
2024-01-03 12:25:09 +0900

    이미지 업로드 기능 추가 - 명언카드 편집 시 활용하는 기능

--- src/components/UI/carousel/Carousel.tsx
+++ src/components/UI/carousel/Carousel.tsx
@@ -5,13 +5,13 @@ import Autoplay from 'embla-carousel-autoplay'
 import { useState, useCallback } from 'react'
 import Image from 'next/image'
 import { useImageElementStore } from '@/store/store'
+import ImageUploadForm from '../form/ImageUploadForm'
 
 export default function Carousel() {
 
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()])
-    const [images, setImages] = useState(['/images/image1.png', '/images/image2.png', '/images/image3.png', '/images/image4.png', '/images/image5.png', '/images/image6.png', '/images/image7.png'])
+    const [images, setImages] = useState(['/images/image1.png', '/images/image2.png', '/images/image3.png', '/images/image4.png', '/images/image5.png', '/images/image6.png', '/images/image7.png','/images/image8.png','/images/image9.png'])
     const setImageSrc = useImageElementStore((state) => state.setImageSrc)
-    const setImageReset = useImageElementStore((state) => state.setImageReset)
     const scrollPrev = useCallback(() => {
         if (emblaApi) emblaApi.scrollPrev()
     }, [emblaApi])
@@ -29,9 +29,9 @@ export default function Carousel() {
                     return <Image onClick={(e) => {
                         const src = images[i]
                         setImageSrc(src)
-                        setImageReset(false)
-                    }} className='rounded-[1em] mx-[10px] shadow-custom1 hover:cursor-pointer' key={image} src={image} alt='명언 카드 배경 이미지' width={230} height={230}></Image>
+                    }} className='rounded-[1em] mx-[10px] shadow-custom1 hover:cursor-pointer bg-[#4ff44f]' key={image} src={image} alt='명언 카드 배경 이미지' width={230} height={230}></Image>
                 })}
+            <ImageUploadForm setImages={setImages} images={images}/>
             </figure>
             {/* 버튼 컨테이너 */}
             <article className='flex justify-center items-center mb-[3em] text-white'>
