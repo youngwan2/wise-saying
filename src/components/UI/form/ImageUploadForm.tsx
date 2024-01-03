995f017d9de0feb39258a76b99e6f53acc78ccb7
Kim Young Wan <qodna25@gmail.com>
2024-01-03 12:37:54 +0900

    fix : 이미지 업로드 기능에서 업로드된 이미지를 blob 로 읽어서 src에 저장하도록 수정
    Update README.md

--- src/components/UI/form/ImageUploadForm.tsx
+++ src/components/UI/form/ImageUploadForm.tsx
@@ -8,11 +8,11 @@ interface PropsType {
 }
 export default function ImageUploadForm({ setImages, images }: PropsType) {
 
-
     const imageSrcReader = (e: ChangeEvent<HTMLInputElement>) => {
         try {
             if (e.target && e.target.files) {
-                const imageSrc = `/images/${e.target.files[0].name}`
+                const files = e.target.files[0]
+                const imageSrc = URL.createObjectURL(files)
                 setImages([...images, imageSrc])
             }
         } catch (error) {
