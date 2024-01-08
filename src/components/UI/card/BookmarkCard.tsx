6a6a64eedd80d2266418a98661dccf6e5aa1968a
Kim Young Wan <qodna25@gmail.com>
2024-01-08 15:34:48 +0900

    북마크 링크 기능 추가

---
+++ src/components/UI/card/BookmarkCard.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import BookmarkDeleteButton from "../button/BookmarkDeleteButton";
+import { HiLink } from "react-icons/hi2";
+
+interface PropsType {
+    key: number
+    bookmark: {
+        id: number
+        category: string
+        wise_sayings: string
+        author: string
+        url: string
+    }
+}
+export default function BookmarkCard({key, bookmark }: PropsType) {
+
+
+    return (
+        <ul className="flex flex-col bg-[white] max-w-[1200px] w-full p-[1em] pt-[2em] m-[5px] rounded-[10px] subpixel-antialiased  relative" key={key}>
+            <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] ">{bookmark.id}</li>
+            <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] ">{bookmark.category}</li>
+            <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] ">{bookmark.author}</li>
+            <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] ">{bookmark.wise_sayings}</li>
+            <li className="text-[#f77b65] text-[1.5em] absolute right-[2em] top-[3px] hover:shadow-[0_0_1px_1px_tomato] " ><Link href={`${bookmark.url}`}><HiLink/></Link></li>
+            <li><BookmarkDeleteButton id={bookmark.id} /></li>
+        </ul>
+    )
+}
