8a71a1299df068d985957d97b7e78cd01da19b69
Kim Young Wan <qodna25@gmail.com>
2024-01-11 17:00:04 +0900

    북마크 리스트 key undefined 에러 수정

--- src/components/UI/card/BookmarkCard.tsx
+++ src/components/UI/card/BookmarkCard.tsx
@@ -3,7 +3,6 @@ import BookmarkDeleteButton from "../button/BookmarkDeleteButton";
 import { HiLink } from "react-icons/hi2";
 
 interface PropsType {
-    key: number
     bookmark: {
         id: number
         category: string
@@ -12,16 +11,14 @@ interface PropsType {
         url: string
     }
 }
-export default function BookmarkCard({key, bookmark }: PropsType) {
-
-
+export default function BookmarkCard({ bookmark }: PropsType) {
     return (
-        <ul className="flex flex-col bg-[white] max-w-[1200px] w-full p-[1em] pt-[2em] m-[5px] rounded-[10px] subpixel-antialiased  relative" key={key}>
+        <ul className="flex flex-col bg-[white] max-w-[1200px] w-full p-[1em] pt-[2em] m-[5px] rounded-[10px] subpixel-antialiased  relative" key={bookmark.id}>
             <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] "><span className="inline-block min-w-[50px] tracking-[6px]">식별| </span>{bookmark.id}</li>
             <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] "><span className="inline-block min-w-[50px] tracking-[5px]">구분| </span>{bookmark.category}</li>
             <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] "><span className="inline-block min-w-[50px] tracking-[5px]">저자| </span>{bookmark.author}</li>
             <li className="mx-[5px] font-bold p-[7px] border border-[#454444a0] "><span className="inline-block min-w-[50px] tracking-[5px]">명언| </span>{bookmark.wise_sayings}</li>
-            <li className="text-[#f77b65] text-[1.5em] absolute right-[2em] top-[3px] hover:shadow-[0_0_1px_1px_tomato] " ><Link href={`${bookmark.url}`}><HiLink/></Link></li>
+            <li className="text-[#f77b65] text-[1.5em] absolute right-[2em] top-[3px] hover:shadow-[0_0_1px_1px_tomato] " ><Link href={`${bookmark.url}`}><HiLink /></Link></li>
             <li><BookmarkDeleteButton id={bookmark.id} /></li>
         </ul>
     )
