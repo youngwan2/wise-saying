96ffe34ab8ad59bfc93176a039c6445e1d36a62e
Kim Young Wan <qodna25@gmail.com>
2024-01-11 17:56:13 +0900

    마이페이지 프로필 카드 스타일 수정

--- src/components/UI/styler/QuotesSizeStyler.tsx
+++ src/components/UI/styler/QuotesSizeStyler.tsx
@@ -17,19 +17,21 @@ export default function QuotesSizeStyler({selectTapNum}:PropsType) {
 
     useEffect(() => {
         setSizeStore(size)
-        console.log(size)
     }, [setSizeStore, size])
 
     return (
             <article className={`${selectTapNum===2? 'block':'hidden'} transition-all`}>
                 <h2 className="flex items-center text-[1.2em] mt-[1.25em] pb-[0.25em] text-[white]"><HiScissors color="white" /> <p className="ml-[0.5em]">카드 크기 변경</p></h2>
                 <div className="flex items-center text-center">
-                    <input aria-label="카드 넓이 사용자 입력창" className="p-[5px] mb-[0.5em] w-[200px] rounded-[10px] " type="number" placeholder="넓이(기본: 350px)" onChange={(e) => {
+                    {/* 캔버스 넓이 */}
+                    <input aria-label="카드 넓이 사용자 입력창" className="p-[5px] mb-[0.5em] w-[150px] rounded-[10px] " type="number" placeholder="넓이(기본: 350px)" onChange={(e) => {
                         const width = Number(e.currentTarget.value)
                         setSize({ ...size, width })
                     }}></input>
-                    <HiX className="text-[1.5em] ml-[5px] pb-[5px]" color="white" />
-                    <input aria-label="카드 눂이 사용자 입력창" className="ml-[3px] p-[5px] mb-[0.5em] w-[200px] rounded-[10px]" type="number" placeholder="높이(기본: 400px)" onChange={(e) => {
+                    <HiX className="text-[2em] ml-[5px] pb-[5px]" color="white" />
+
+                    {/* 캔버스 높이 */}
+                    <input aria-label="카드 눂이 사용자 입력창" className="ml-[3px] p-[5px] mb-[0.5em] w-[150px] rounded-[10px]" type="number" placeholder="높이(기본: 400px)" onChange={(e) => {
                         const height = Number(e.currentTarget.value)
                         setSize({ ...size, height })
                     }}></input>
