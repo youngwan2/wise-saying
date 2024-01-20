876853858f3e3db831a6db52f65aae9a5a328c02
Kim Young Wan <qodna25@gmail.com>
2024-01-20 12:38:12 +0900

    deploy/테스트배포:vercel

--- src/services/item.get.ts
+++ src/services/item.get.ts
@@ -65,7 +65,7 @@ export const getBookmarkListFormDB = async (
 
 /**
  * * GET | 각 페이지의 카테고리별 메타데이터 불러오기
- * @param path1 중분류(ex. authors, days, etc, users)
+ * @param path1 중분류(ex. authors, days, etc, users, weathers)
  * @param path2 소분류(ex. 소크라테스, 월, 사랑, 인생)
  * @returns
  * @example `http://localhost:3000/api/items/${path1}/${path2}}`
@@ -76,9 +76,11 @@ export const getApiMetaDataFromServer = async (
   path2: string,
 ) => {
   try {
-    const response = await fetch(
-      `http://localhost:3000/api/items/${path1}/${path2}?type=meta`,
-    )
+    const url = path1 === 'weathers'
+      ? `http://localhost:3000/api/items/${path1}?type=meta`
+      : `http://localhost:3000/api/items/${path1}/${path2}?type=meta`
+    console.log('GET 메타 데이터 분기 경로:',url)
+    const response = await fetch(url)
     const result = await response.json()
 
     return result
