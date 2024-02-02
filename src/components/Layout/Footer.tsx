b659eff4d2585faa4a8c1c29288b49127fed7a0e
Kim Young Wan <qodna25@gmail.com>
2024-02-02 20:56:41 +0900

    fix/Header 경로 문제로 인한 빌드 에러 수정

--- src/components/Layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <footer className="shadow-[inset_-2px_-2px_5px_rgba(0,0,0,0.5)] top-0 z-20 max-h-[45px] min-h-[50px] mb-[5px] mx-auto max-w-[1700px] flex justify-between bg-[#fcfbfb] p-[15px]  w-[100%] px-[10px] items-center rounded-b-[5px] ">
-      <h1 className="p-[5px] text-[1.25em] font-bold">Wise Sayings</h1>
-    </footer>
-  )
-}
