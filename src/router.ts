195d6e63c411dc2a266d66f586cb6db0b2b09218
Kim Young Wan <qodna25@gmail.com>
2024-02-29 18:15:10 +0900

    feature/ Home 페이지 구조 변경 및 기능 확장 - 기존에는 사용자가 home(root) 페이지 접속 시 오늘의 명언 목록만 보여주었으나 사용자의 접근성을 위해 카테고리 목록을 추가로 제공토록 기능을 확장

---
+++ src/router.ts
@@ -0,0 +1,20 @@
+import {
+    HiNewspaper,
+    HiOutlineHandThumbUp,
+    HiOutlineHomeModern,
+    HiOutlinePencil,
+    HiOutlineUserGroup,
+  } from 'react-icons/hi2'
+  import { HiPhotograph } from 'react-icons/hi'
+  
+
+const navList = [
+    { path: '/quotes/topics', label: '주제별 명언', icon: HiNewspaper },
+    { path: '/quotes/authors', label: '인물별 명언', icon: HiOutlineUserGroup },
+    { path: '/user-quotes', label: '유저 명언', icon: HiOutlineHandThumbUp },
+    { path: '/add-wisesaying', label: '명언 쓰기', icon: HiOutlinePencil },
+    { path: '/mypage', label: '마이페이지', icon: HiPhotograph },
+    { path: '/', label: '홈', icon: HiOutlineHomeModern },
+  ]
+
+export default navList
