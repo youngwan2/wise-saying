ba8333ed785009ec7817e3319d0063e1014154bc
Kim Young Wan <qodna25@gmail.com>
2024-03-07 10:39:25 +0900

    fix/cors 문제 해결을 위한 next.config.js 파일 내 headers 옵션 추가
    - https://nextjs.org/docs/app/api-reference/next-config-js/headers#cors 을 참고하여 사이트 도메인에 대한 cors 허용 도메인 설정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 
+const { headers } = require('next/headers')
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
@@ -20,6 +22,28 @@ const nextConfig = {
     ],
     
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "www.wise-sayings.com", //
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 }
+
 
 module.exports = withBundleAnalyzer(nextConfig)
