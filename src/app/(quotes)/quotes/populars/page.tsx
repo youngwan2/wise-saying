c37ae2118fb271608a5f9b7b499eb20064d78e6b
Kim Young Wan <qodna25@gmail.com>
2024-03-26 12:34:40 +0900

    fix/실시간 인기명언 및 오늘의 명언 페이지의 갱신이 안 되는 문제 수정

--- src/app/(quotes)/quotes/populars/page.tsx
+++ src/app/(quotes)/quotes/populars/page.tsx
@@ -1,9 +1,10 @@
-'use client'
-import ReplaceMessageCard from "@/components/UI/common/ReplaceMessageCard"
-import PopularQuoteList from "@/components/UI/quote/PopularQuoteList"
-import { useSwrFetch } from "@/utils/swr"
-import {toast} from "react-hot-toast"
-import { HiRefresh } from "react-icons/hi"
+import PopularQuoteContainer from "@/components/UI/quote/PopularQuoteContainer"
+import { Metadata } from "next"
+
+export const metadata:Metadata= {
+    title:'실시간 인기명언',
+    description:'조회수가 높은 명언목록 100개를 실시간으로 조회하여 보여주는 페이지입니다.'
+}
 
 export interface QuoteType {
     quote_id: number
@@ -15,26 +16,12 @@ export interface QuoteType {
 }
 export default function PopularQuotesPage() {
 
-    const url = '/api/quotes/populars'
-    const { data, mutate } = useSwrFetch(url,15000, false)
-    const { quotes } = data || { quotes: null }
-
-    async function reload() {
-        toast.promise(mutate(), {
-            loading:'새로운 데이터를 불러오는 중입니다.',
-            success:'최신 데이터로 갱신되었습니다.',
-            error:'서버 문제로 데이터 조회에 실패하였습니다.'
-        })
-    }
-
-    if (!quotes) return <ReplaceMessageCard childern='데이터를 조회중 입니다..' />
     return (
         <article>
             <h2 className="flex flex-col  justify-center items-center text-[1.5em] p-[10px]  text-center text-white max-w-[250px] mx-auto bg-gradient-to-b from-[transparent] to-[#00000033]  shadow-[0_9px_2px_0_rgba(0,0,0,0.5)] rounded-[5px] my-[2em] perspective-500  ">
                 실시간 인기명언 <span> Top 100</span>
             </h2>
-            <button className="absolute flex items-center left-[50%] translate-x-[-50%] text-white hover:cursor-pointer z-[20] hover:text-[gold] " onClick={reload}><HiRefresh className='mx-[3px]' />새로고침 </button>
-            <PopularQuoteList quotes={quotes} />
+            <PopularQuoteContainer />
         </article>
 
     )
--- src/app/api/quotes/populars/route.ts
+++ src/app/api/quotes/populars/route.ts
@@ -1,24 +1,24 @@
+export const dynamic = 'force-dynamic' 
+
 import { HTTP_CODE } from "@/app/http-code"
 import { openDB } from "@/utils/connect";
 import { NextResponse } from "next/server"
 
 export async function GET() {
-
     try {
         const db = await openDB();
 
         const selectQuery = `
-        SELECT * 
-        FROM quotes A 
+        SELECT *  FROM quotes A 
         INNER JOIN views B ON A.quote_id = B.quote_id
         ORDER BY B.views DESC, A.quote_id ASC
         `
 
         const results = await db.query(selectQuery)
         const items = results.rows
+        db.end()
+        return NextResponse.json({ ...HTTP_CODE.OK, quotes: items })
 
-        return NextResponse.json({...HTTP_CODE.OK,quotes:items})
-        
     } catch (error) {
         console.error('/api/quotes/populars', error)
         return NextResponse.json(HTTP_CODE.INTERNAL_SERVER_ERROR)
