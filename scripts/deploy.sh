03041123dc4e3f35524363fd9d2e198b05649c60
Kim Young Wan <qodna25@gmail.com>
2024-06-21 18:00:06 +0900

    deploy.sh 패키지 환경변수 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 # project.zip 압축풀기가 완료된 소스파일들이 모인 경로로 이동합니다.
 cd /home/ec2-user/project
 
+# 패키지가 설치된 경로의 환경변수 추가
+export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v20.11.0/bin
+
 # 종속성이 있으면 설치합니다.
 npm install
 
