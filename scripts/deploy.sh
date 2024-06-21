2ba2bf13681e0e6ce59913091c9afc4d175ea5b4
Kim Young Wan <qodna25@gmail.com>
2024-06-21 16:23:44 +0900

    깃허브 액션 빌드 캐싱 작업 추가 및 appspec.yml 파일 소스의 사용자 권한부여 옵션 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-# project.zip 설치 후 해당 파일이 있는 경로로 이동합니다.
-cd /home/ec2-user
 
-# project.zip 의 압축을 현재 경로에 풉니다.
-unzip project.zip -d project
-
-# project 폴더로 들어갑니다.
-cd project
+# project.zip 압축풀기가 완료된 소스파일들이 모인 경로로 이동합니다.
+cd /home/ec2-user/project
 
 # 종속성이 있으면 설치합니다.
 npm install
 
-# 서버를 재시작 합니다.
+# 서버를 재시작 합니다 -> 여기서 'start'는 사용자 작명입니다. 
+# pm2 에 대한 자세한 부분은 npm pm2 를 찾아보세요. 
 pm2 restart 'start'
